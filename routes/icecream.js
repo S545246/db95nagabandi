41052e4881298ba924102d0c187e01fb798ab86a
nagabandinikhil <43547232+nagabandinikhil@users.noreply.github.com>
2021-11-17 17:47:01 -0600

    changes made to the icecreamdelete.pug

--- routes/icecream.js
+++ routes/icecream.js
@@ -15,4 +15,6 @@ router.get('/create', icecream_controlers.icecream_create_Page);
 /* GET create update page */
 router.get('/update', icecream_controlers.icecream_update_Page);
 
+/* GET create costume page */
+router.get('/delete', icecream_controlers.icecream_delete_Page);
 module.exports = router;
